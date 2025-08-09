863911f138b3d0e934eced79ac4693afaf24e080
nt-thuyr <ngthanhthuy127@gmail.com>
2025-08-09 13:58:56 +0700

    fix models, forms, urls

--- map_web/urls.py
+++ map_web/urls.py
@@ -1,57 +1,14 @@
-"""
-
-URL configuration for map_web project.
-
-
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-
-https://docs.djangoproject.com/en/5.2/topics/http/urls/
-
-Examples:
-
-Function views
-
-1. Add an import: from my_app import views
-
-2. Add a URL to urlpatterns: path('', views.home, name='home')
-
-Class-based views
-
-1. Add an import: from other_app.views import Home
-
-2. Add a URL to urlpatterns: path('', Home.as_view(), name='home')
-
-Including another URLconf
-
-1. Import the include() function: from django.urls import include, path
-
-2. Add a URL to urlpatterns: path('blog/', include('blog.urls'))
-
-"""
-
-from django.contrib import admin
-
+# urls.py
 from django.urls import path
-
-from chamu.views import (
-
-homepage,
-
-evaluation_survey_view,
-
-)
-
-
+from django.contrib import admin
+from chamu import views
 
 urlpatterns = [
-
-path('admin/', admin.site.urls),
-
-path('', homepage, name='homepage'),
-
-# path('matching_survey/', matching_survey, name='matching_survey'),
-
-path('evaluation_survey/<int:user_info_id>/', evaluation_survey_view, name='evaluation_survey'),
-
+    path('admin/', admin.site.urls),
+    path('', views.homepage, name='homepage'),
+    path('evaluation/<int:user_info_id>/', views.evaluation_survey_view, name='evaluation_survey'),
+    path('matching/<int:user_info_id>/', views.matching_survey_view, name='matching_survey'),
+    path('results/<int:user_info_id>/', views.matching_results_view, name='matching_results'),
+    path('api/matching-details/<int:user_info_id>/<int:province_id>/',
+         views.get_province_matching_details, name='matching_details_api'),
 ]
