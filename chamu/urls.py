703e8e32d6051d3eddb5444c01b4f0500cf71474
nt-thuyr <ngthanhthuy127@gmail.com>
2025-08-12 09:59:08 +0700

    fix urls

--- chamu/urls.py
+++ chamu/urls.py
@@ -1,9 +1,7 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.homepage, name='homepage'),
     path('ajax/login/', views.ajax_login, name='ajax_login'),
     path('ajax/signup/', views.ajax_signup, name='ajax_signup'),
@@ -11,5 +9,7 @@ urlpatterns = [
     path('ajax/logout/', views.ajax_logout, name='ajax_logout'),
     path('evaluation/<int:user_info_id>/', views.evaluation_survey_view, name='evaluation_survey'),
     path('matching/<int:user_info_id>/', views.matching_survey_view, name='matching_survey'),
-    path('matching/<int:user_info_id>/results/', views.matching_results_view, name='matching_results'),
+    path('results/<int:user_info_id>/', views.matching_results_view, name='matching_results'),
+    # path('api/matching-details/<int:user_info_id>/<int:province_id>/',
+    #      views.get_province_matching_details, name='matching_details_api'),
 ]
--- map_web/urls.py
+++ map_web/urls.py
@@ -1,14 +1,8 @@
 # urls.py
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
-from chamu import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage, name='homepage'),
-    path('evaluation/<int:user_info_id>/', views.evaluation_survey_view, name='evaluation_survey'),
-    path('matching/<int:user_info_id>/', views.matching_survey_view, name='matching_survey'),
-    path('results/<int:user_info_id>/', views.matching_results_view, name='matching_results'),
-    # path('api/matching-details/<int:user_info_id>/<int:province_id>/',
-    #      views.get_province_matching_details, name='matching_details_api'),
+    path('chamu/', include('chamu.urls')),
 ]
