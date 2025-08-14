b47f4c683dc07b08b2a21dd630466bc8dbe0cb46
nt-thuyr <ngthanhthuy127@gmail.com>
2025-08-14 10:40:18 +0700

    fe starter

--- map_web/settings.py
+++ map_web/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',   # for customizing form widgets in templates
     'chamu',  # name of app
 ]
 
